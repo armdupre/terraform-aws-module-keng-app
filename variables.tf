ca8c77f7596cd9ac052f0ddb6145dd60b9309675
armdupre <a-joseph.dupre@keysight.com>
2025-05-21 00:06:08 -0400

    Updated.

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "AmiName" {
-	default = "ubuntu-pro-server/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-pro-server-20250325"
+	default = "ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20250516-47489723-7305-4e22-8b22-b0d57054f216"
 	description = "AMI name used for deploying instances"
 	type = string
 }
 
 variable "AmiOwner" {
-	default = "099720109477"
+	default = "679593333241"
 	description = "Owner of AMI used for deploying instances"
 	type = string
 }
